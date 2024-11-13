b925c4067be2e94e2b57af6bb4d5157d5488a644
Reponse2024 <reponse.iduha2023@kepler.org>
2024-11-13 14:25:31 +0000

    A new file the checks the sign of a product of three intergers, and print it on the console

---
+++ 01-fundamentals/01-Conditionals/condition.js
@@ -0,0 +1,18 @@
+ let a= 5;
+ let b= -7;
+ let c= 8;
+ if(a>0 && b>0 && c>0){
+    console.log('the sign is +');
+ }
+ else if(a<0 && b<0 && c<0){
+    console.log('the sign is -');
+ }
+ else if(a>0 && b<0 && c<0){
+    console.log('The sign is +');
+ }
+ else if(a<0 && b>0 && c<0){
+    console.log('the sign is +');
+ }
+ else{
+    console.log('the sign is -');
+ }
---
+++ 01-fundamentals/01-Conditionals/largestNumber.js
@@ -0,0 +1,8 @@
+let a= 5;
+ let b= 6;
+if(a>b){
+    console.log(a +" "+ "is the largest number");
+}
+else{
+    console.log(b +" "+ "is the largest number");
+}
