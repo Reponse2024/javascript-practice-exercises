a9ae82f91bbccb239c36a19736991cd260c6a213
Reponse2024 <reponse.iduha2023@kepler.org>
2024-11-14 08:46:51 +0000

    This was the average grade of all the total grades obtaimed

---
+++ 01-fundamentals/01-Conditionals/averageMarks.js
@@ -0,0 +1,18 @@
+let average=(80+77+88+95+68)/5;
+
+if(average >=90 && average <100){
+    console.log('A');
+}
+else if(average>=80){
+    console.log('B');
+}
+else if(average>=70){
+    console.log('C');
+}
+
+else if(average>=60){
+    console.log('D');
+}
+else if(average<60){
+    console.log('F');
+}
