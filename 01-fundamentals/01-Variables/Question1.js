7647e6d53507302e096ac8553aa07761c94b6310
Reponse2024 <reponse.iduha2023@kepler.org>
2024-11-15 08:18:35 +0000

    THe codes that Output your fortune to the screen.

---
+++ 01-fundamentals/01-Variables/Question1.js
@@ -0,0 +1,5 @@
+let child=4;
+let partn= 'John';
+let location= 'Kamonyi';
+let job= 'Software Engineer';
+console.log('I shall be a'+' '+ job +' '+'in'+ ' '+location+','+' '+ 'and married to' +' '+partn +' '+'with'+ ' '+child +' '+'kids.');
